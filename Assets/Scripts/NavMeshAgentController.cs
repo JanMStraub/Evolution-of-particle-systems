119471a0fe10ab28856d8c39ce2bb4fdc848c716
Jan Straub <Straub.JanM@gmail.com>
2021-06-23 10:19:30 +0200

    worked on commute

--- Assets/Scripts/NavMeshAgentController.cs
+++ Assets/Scripts/NavMeshAgentController.cs
@@ -6,42 +6,61 @@ using UnityEngine.AI;
 public class NavMeshAgentController : MonoBehaviour {
 
     [SerializeField]
-    Transform _destination;
+    Transform _workDestination, _homeDestination;
     NavMeshAgent _agent;
     AgentAgendaController _agentAgendaController;
-    int interval = 10;
+    int _pauseTime = 5;
 
     void Start() {
         _agent = this.GetComponent<NavMeshAgent>();
-
+        
         if(_agent == null) {
             Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
         } 
+        
+        StartCoroutine(Commute());
     }
 
-    void Update() {
-        if (Time.frameCount % interval == 0) {
-            _agent.SetDestination(_destination.transform.position);
-            if (reachedDestination()) {
-                Timer(3.0f);
-            }
+    IEnumerator Commute() {
+        while(true) {
+            // Agent work routine
+            workRoutine();
+
+            yield return new WaitUntil(()=>reachedDestination(_workDestination));
+            yield return new WaitForSeconds(_pauseTime);
+
+            // Agent home routine
+            homeRoutine();
+            
+            yield return new WaitUntil(()=>reachedDestination(_homeDestination));
+            yield return new WaitForSeconds(_pauseTime);
         }
     }
 
-    bool reachedDestination() {
-        if (_agent.remainingDistance < _agent.stoppingDistance) {
+    void workRoutine() {
+        Debug.Log(_agent.GetInstanceID() + " work routine");
+        gameObject.SetActive (true);
+        _agent.isStopped = false;
+        _agent.SetDestination(_workDestination.transform.position);
+    }
+
+    void homeRoutine() {
+        Debug.Log(_agent.GetInstanceID() + " home routine");
+        gameObject.SetActive (true);
+        _agent.isStopped = false;
+        _agent.SetDestination(_homeDestination.transform.position);
+    }
+
+    bool reachedDestination(Transform destination) {
+        float distance = Vector3.Distance(_agent.transform.position, destination.transform.position);
+
+        if (distance < _agent.stoppingDistance) {
+            Debug.Log(_agent.GetInstanceID() + " destination reached");
+            gameObject.SetActive (false);
             _agent.isStopped = true;
             return true;
         }
+
         return false;
-    }
-
-    void Timer(float time) {
-        float targetTime = time;
-        targetTime -= Time.deltaTime;
-
-        if (targetTime <= 0.0f) {
-            print("Test");
-        }
     }
 }
