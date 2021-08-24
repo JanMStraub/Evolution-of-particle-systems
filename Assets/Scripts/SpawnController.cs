c081fe0eb49d7fb13991caaf4e14ccf509302c3c
Jan Straub <Straub.JanM@gmail.com>
2021-08-24 14:16:56 +0200

    spawning issue fixed

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -6,47 +6,34 @@ using UnityEngine.AI;
 
 class SpawnController : MonoBehaviour {
 
-    int agentCount = 5;
+    public int agentCount = 10;
 
     public float range = 30f;
     
-    GameObject _Agent;
+    private GameObject _Agent;
 
 
     void Start () {
         _Agent = GameObject.FindGameObjectWithTag("Agent");
         Debug.Log("SpawnAgents");
+        Debug.Log("AgentCount " + agentCount);
         Spawn();
     }
 
 
-    bool RandomPoint(Vector3 center, float range, out Vector3 result) {
-
-        for (int i = 1; i < agentCount; i++) {
-
-            Vector3 randomPoint = center + UnityEngine.Random.insideUnitSphere * range;
-            NavMeshHit hit;
-            
-            if (NavMesh.SamplePosition (randomPoint, out hit, 1.0f, NavMesh.AllAreas)) {
-                result = hit.position;
-                return true;
-            }
-        }
-
-        result = Vector3.zero;
-        return false;
-    }
-
-
     void Spawn() {
-
-        Vector3 point;
         
         for (int i = 1; i < agentCount; i++) {
 
-            if (RandomPoint(_Agent.transform.position, range, out point)) {
-                Instantiate(_Agent, point, transform.rotation);
-            }
+            Vector3 randomPoint = UnityEngine.Random.insideUnitSphere * range;
+            NavMeshHit hit;
+
+            NavMesh.SamplePosition (randomPoint, out hit, range, 1);
+
+            Vector3 point = hit.position;
+            
+            Instantiate(_Agent, point, transform.rotation);
+            Debug.Log("Agent Number: " + i);
         }
         
         GameManager.GameManagerInstance.UpdateGameState(GameState.SetAgentCommute);
