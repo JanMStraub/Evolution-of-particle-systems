7a490ca97a5a35ade13ad99e5a615547a48b5af2
Jan Straub <Straub.JanM@gmail.com>
2021-10-06 19:38:37 +0200

    deleted some plugins

---
+++ Assets/Scripts/Test/Pathcalculation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Pathcalculation {
+    
+    public Pathcalculation(int width, int height) {
+        GridGraph gridGraph = AstarPath.active.data.gridGraph;
+    }
+
+}
